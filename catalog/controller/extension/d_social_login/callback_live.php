a439119f919349b3688b6269a26cf6795b94c585
Pavel Poimanov <php5.drvn@gmail.com>
2018-01-17 15:23:10 +0200

    hybrid lib fix

--- catalog/controller/extension/d_social_login/callback_live.php
+++ catalog/controller/extension/d_social_login/callback_live.php
@@ -8,8 +8,8 @@ class ControllerExtensionDSocialLoginCallbackLive extends Controller {
 	public function index (){
 		$_REQUEST['hauth_done'] = 'Live';
 
-		require_once("system/library/hybrid/auth.php");
-		require_once("system/library/hybrid/endpoint.php");
+		require_once("system/library/d_social_login/hybrid/auth.php");
+		require_once("system/library/d_social_login/hybrid/endpoint.php");
 		Hybrid_Endpoint::process();
 	}
 }
