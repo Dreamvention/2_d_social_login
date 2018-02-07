60a8e6cdbf8756f47e99218f5e1401734973e89d
Pavel Poimanov <php5.drvn@gmail.com>
2018-02-07 10:29:44 +0200

    yahoo fix was openId - now oauth 2.0

--- system/config/d_social_login/yahoo.php
+++ system/config/d_social_login/yahoo.php
@@ -3,12 +3,13 @@
 $_['d_social_login_yahoo'] = array(
     "Yahoo" => array(
         "enabled" => true,
-        "keys" => array("key" => "", "secret" => ""),
+        "keys"    => array("id" => "", "secret" => ""),
         "id" => 'yahoo',
         "sort_order" => 11,
         "icon" => 'dsl-yahoo',
         "background_color" => '#500095',
         "background_color_active" => '#3d026f',
         "background_color_hover" => '#3d026f',
-    )
+    ),
+
 );
