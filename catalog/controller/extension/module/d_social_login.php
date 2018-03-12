218019d9596232829b0997619ccd7c1256b168bf
pavel <php5.drvn@gmail.com>
2018-03-12 16:59:16 +0200

    fix bugs

--- catalog/controller/extension/module/d_social_login.php
+++ catalog/controller/extension/module/d_social_login.php
@@ -28,7 +28,6 @@ class ControllerExtensionModuleDSocialLogin extends Controller
         $this->document->addStyle('catalog/view/theme/default/stylesheet/d_social_login/styles.css');
         $this->document->addScript('catalog/view/javascript/d_social_login/spin.min.js');
 
-        $this->initializeSlRedirect();
         $setting = $this->config->get($this->codename . '_setting');
 
         //load data from provider into form popup
@@ -70,6 +69,7 @@ class ControllerExtensionModuleDSocialLogin extends Controller
         if (!isset($this->session->data['sl_redirect'])) {
             $this->session->data['sl_redirect'] = ($setting['return_page_url']) ? $setting['return_page_url'] : $this->model_extension_module_d_social_login->getCurrentUrl();
         }
+        $data['url'] = $this->model_extension_module_d_social_login->getCurrentUrl();
 
         // facebook fix
         unset($this->session->data['HA::CONFIG']);
@@ -80,7 +80,9 @@ class ControllerExtensionModuleDSocialLogin extends Controller
     public function login()
     {
         // multistore fix
-        $this->initializeSlRedirect();
+
+            $this->initializeSlRedirect();
+
         $this->load->model('setting/store');
         $stores = $this->model_setting_store->getStores();
         $store_id = $this->config->get('config_store_id');
@@ -125,7 +127,8 @@ class ControllerExtensionModuleDSocialLogin extends Controller
             $remoteLoginResponce['scripts'] = $this->document->getScripts();
             $remoteLoginResponce['styles'] = $this->document->getStyles();
             $remoteLoginResponce['pre_loader'] = $this->model_extension_module_d_social_login->getPreloader();
-            $remoteLoginResponce['url'] = $this->model_extension_module_d_social_login->getCurrentUrl(false);
+            $remoteLoginResponce['url'] = $this->sl_redirect;//fix
+
             $view = $this->model_extension_d_opencart_patch_load->view($this->codename . '/auth', $remoteLoginResponce);
             $this->response->setOutput($view);
         } catch (Exception $e) {
@@ -198,12 +201,12 @@ class ControllerExtensionModuleDSocialLogin extends Controller
         $this->response->addHeader('Content-Type: application/json');
         $this->response->setOutput(json_encode($json));
     }
-
     /*
      * Called when user close pop up window
      * this method reset provider so pop up will not be showen with
      * same  auth data but with same user
      * */
+
     public function reset()
     {
         unset($this->session->data['provider']);
@@ -211,69 +214,31 @@ class ControllerExtensionModuleDSocialLogin extends Controller
         $this->response->setOutput($this->index());
     }
 
-    private function getForm($customer_data, $authentication_data)
+    private function initializeSlRedirect()
     {
-        $this->document->addStyle('catalog/view/theme/default/stylesheet/d_social_login/pre_loader/' . 'clip-rotate' . '.css');
-        $this->document->addStyle('catalog/view/theme/default/stylesheet/d_social_login/form.css' . '?' . rand());
-        $this->document->addScript('catalog/view/javascript/d_social_login/jquery.validate.js');
-        $this->document->addScript('catalog/view/javascript/d_social_login/jquery.maskedinput.min.js');
+        $this->session->data['reset'] = false;
+        // correct &amp; in url
+        if (isset($this->request->get)) {
 
-        $data['debug'] = $this->setting['debug_mode'] = (bool)$this->setting['debug_mode'];
-        $data['islogged'] = ($this->customer->isLogged()) ? $this->customer->isLogged() : false;
-        $data['pre_loader'] = html_entity_decode($this->model_extension_module_d_social_login->getPreloader('clip-rotate'), ENT_QUOTES, 'UTF-8');
-        $useragent=$_SERVER['HTTP_USER_AGENT'];
-        $data['mobile']=preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i',$useragent)||preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i',substr($useragent,0,4));
-
-        $this->session->data['customer_data'] = $customer_data;
-        $this->session->data['authentication_data'] = $authentication_data;
-
-        $data['customer_data'] = $customer_data;
-        $data['authentication_data'] = $authentication_data;
-        $data['text_email_intro'] = $this->language->get('text_email_intro');
-        $data['text_help_required'] = $this->language->get('text_help_required');
-        $data['button_sign_in_mail'] = $this->language->get('button_sign_in_mail');
-        $data['button_sign_in'] = $this->language->get('button_sign_in');
-
-        $labels_fields = array();
-
-        foreach (array_keys($this->setting['fields']) as $key) {
-            $labels_fields[$key] = $this->language->get('text_entry_' . $key);
-            $data["text_".$key]=$this->language->get('text_' . $key);//load txt
-        }
-        $data['labels_field'] = $labels_fields;
-        $data['background'] = $is_background = !$this->setting['iframe'];
-        $background_image = $this->setting['background_img'];
-        if ($is_background) {
-            $this->load->model('tool/image');
-            if (isset($background_image) && $background_image
-                && file_exists(DIR_IMAGE . $background_image) && is_file(DIR_IMAGE . $background_image)) {
-                $data['background_img_thumb'] = $this->model_tool_image->resize($background_image,
-                    $this->setting['background_img']['width'], $background_image, $this->setting['background_img']['height']);
-            } else {
-                $data['background_img_thumb'] = $this->model_tool_image->resize('no_image.jpg',
-                    $this->setting['background_img']['width'], $background_image, $this->setting['background_img']['height']);
+            foreach ($this->request->get as $key => $value) {
+                $this->request->get[str_replace('amp;', '', $key)] = $value;
             }
         }
 
-        $sort_order = array();
-        foreach ($this->setting['fields'] as $key => $value) {
-            if (isset($value['sort_order'])) {
-                $sort_order[$key] = $value['sort_order'];
+        if (isset($_GET)) {
+            foreach ($_GET as $key => $value) {
+                $_GET[str_replace('amp;', '', $key)] = $value;
             }
         }
-        array_multisort($sort_order, SORT_ASC, $this->setting['fields']);
-        $data['fields'] = $this->setting['fields'];
-        $provider=$this->setting['providers'][$this->session->data['provider']];
-        $data['log_style']=' background-image: url(\'catalog/view/theme/default/stylesheet/d_social_login/icons/'.$provider['id'].'.svg\');background-color:'.$provider['background_color'];
-        $this->load->model('localisation/country');
-        $data['countries'] = $this->model_localisation_country->getCountries();
-        return $this->model_extension_d_opencart_patch_load->view($this->codename . '/form', $data);
-    }
 
-    private function getConfirmMessageView()
-    {
-        $data['text_confirm'] = $this->language->get('text_confirm_finish');
-        return $this->model_extension_d_opencart_patch_load->view($this->codename . '/confirm', $data);
+        // set redirect address
+        if (isset($this->session->data['sl_redirect']) && !stripos($this->session->data['sl_redirect'], 'logout')) {
+            $this->sl_redirect = $this->session->data['sl_redirect'];
+        } else {
+            $this->sl_redirect = $this->url->link('account/account', '', 'SSL');
+        }
+
+
     }
 
     private function validateRegistration($customer_data)
@@ -307,30 +272,70 @@ class ControllerExtensionModuleDSocialLogin extends Controller
 
     }
 
-    private function initializeSlRedirect()
+
+    private function getConfirmMessageView()
     {
-        // correct &amp; in url
-        if (isset($this->request->get)) {
+        $data['text_confirm'] = $this->language->get('text_confirm_finish');
+        return $this->model_extension_d_opencart_patch_load->view($this->codename . '/confirm', $data);
+    }
 
-            foreach ($this->request->get as $key => $value) {
-                $this->request->get[str_replace('amp;', '', $key)] = $value;
+    private function getForm($customer_data, $authentication_data)
+    {
+        $this->document->addStyle('catalog/view/theme/default/stylesheet/d_social_login/pre_loader/' . 'clip-rotate' . '.css');
+        $this->document->addStyle('catalog/view/theme/default/stylesheet/d_social_login/form.css' . '?' . rand());
+        $this->document->addScript('catalog/view/javascript/d_social_login/jquery.validate.js');
+        $this->document->addScript('catalog/view/javascript/d_social_login/jquery.maskedinput.min.js');
+
+        $data['debug'] = $this->setting['debug_mode'] = (bool)$this->setting['debug_mode'];
+        $data['islogged'] = ($this->customer->isLogged()) ? $this->customer->isLogged() : false;
+        $data['pre_loader'] = html_entity_decode($this->model_extension_module_d_social_login->getPreloader('clip-rotate'), ENT_QUOTES, 'UTF-8');
+        $useragent = $_SERVER['HTTP_USER_AGENT'];
+        $data['mobile'] = preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i', $useragent) || preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i', substr($useragent, 0, 4));
+
+        $this->session->data['customer_data'] = $customer_data;
+        $this->session->data['authentication_data'] = $authentication_data;
+
+        $data['customer_data'] = $customer_data;
+        $data['authentication_data'] = $authentication_data;
+        $data['text_email_intro'] = $this->language->get('text_email_intro');
+        $data['text_help_required'] = $this->language->get('text_help_required');
+        $data['button_sign_in_mail'] = $this->language->get('button_sign_in_mail');
+        $data['button_sign_in'] = $this->language->get('button_sign_in');
+
+        $labels_fields = array();
+
+        foreach (array_keys($this->setting['fields']) as $key) {
+            $labels_fields[$key] = $this->language->get('text_entry_' . $key);
+            $data["text_" . $key] = $this->language->get('text_' . $key);//load txt
+        }
+        $data['labels_field'] = $labels_fields;
+        $data['background'] = $is_background = !$this->setting['iframe'];
+        $background_image = $this->setting['background_img'];
+        if ($is_background) {
+            $this->load->model('tool/image');
+            if (isset($background_image) && $background_image
+                && file_exists(DIR_IMAGE . $background_image) && is_file(DIR_IMAGE . $background_image)) {
+                $data['background_img_thumb'] = $this->model_tool_image->resize($background_image,
+                    $this->setting['background_img']['width'], $background_image, $this->setting['background_img']['height']);
+            } else {
+                $data['background_img_thumb'] = $this->model_tool_image->resize('no_image.jpg',
+                    $this->setting['background_img']['width'], $background_image, $this->setting['background_img']['height']);
             }
         }
 
-        if (isset($_GET)) {
-            foreach ($_GET as $key => $value) {
-                $_GET[str_replace('amp;', '', $key)] = $value;
+        $sort_order = array();
+        foreach ($this->setting['fields'] as $key => $value) {
+            if (isset($value['sort_order'])) {
+                $sort_order[$key] = $value['sort_order'];
             }
         }
-
-        // set redirect address
-        if (isset($this->session->data['sl_redirect']) && !stripos($this->session->data['sl_redirect'], 'logout')) {
-            $this->sl_redirect = $this->session->data['sl_redirect'];
-        } else {
-            $this->sl_redirect = $this->url->link('account/account', '', 'SSL');
-        }
-
-
+        array_multisort($sort_order, SORT_ASC, $this->setting['fields']);
+        $data['fields'] = $this->setting['fields'];
+        $provider = $this->setting['providers'][$this->session->data['provider']];
+        $data['log_style'] = ' background-image: url(\'catalog/view/theme/default/stylesheet/d_social_login/icons/' . $provider['id'] . '.svg\');background-color:' . $provider['background_color'];
+        $this->load->model('localisation/country');
+        $data['countries'] = $this->model_localisation_country->getCountries();
+        return $this->model_extension_d_opencart_patch_load->view($this->codename . '/form', $data);
     }
 
 
