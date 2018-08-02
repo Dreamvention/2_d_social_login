dabad606f996f0dfa1e5823f30054298431414bc
pavel <php5.drvn@gmail.com>
2018-08-02 10:28:51 +0300

    delete facebook sorry man :((( buy full module :P

---
+++ system/config/d_social_login/steam.php
@@ -0,0 +1,14 @@
+<?php
+
+$_['d_social_login_steam'] = array(
+    "Steam" => array (
+        "enabled" => true,
+        "keys"    => array ( "id" => "", "key" => "" ),
+        "id"  => 'steam',
+        "sort_order" => 15,
+        "icon" => 'dsl-steam',
+        "background_color" => '#171a21',
+        "background_color_active" => '#000000',
+        "background_color_hover" => '#000000',
+    ),
+);
