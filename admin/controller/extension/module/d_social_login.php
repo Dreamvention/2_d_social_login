a03677734f71cfa7bfb7f69ecfdb3c59ecbfc8aa
dmitriyzhuk <dmitriyzhuk@gmail.com>
2018-09-29 18:43:58 +0300

    fix

--- admin/controller/extension/module/d_social_login.php
+++ admin/controller/extension/module/d_social_login.php
@@ -373,6 +373,11 @@ class ControllerExtensionModuleDSocialLogin extends Controller
         }
 
         $setting = array_replace_recursive($config, $setting);
+        foreach($setting['providers'] as $provider_id => $provider){
+            if(!isset($config['providers'][$provider_id])){
+                unset($setting['providers'][$provider_id]);
+            }
+        }
         $setting['sizes'] = $config['sizes'];
         return $setting;
     }
