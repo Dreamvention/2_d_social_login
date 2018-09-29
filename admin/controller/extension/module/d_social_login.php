4d455f2e93a0fde9e98eaabe7648721028ba78d1
dmitriyzhuk <dmitriyzhuk@gmail.com>
2018-09-29 18:40:10 +0300

    fix

--- admin/controller/extension/module/d_social_login.php
+++ admin/controller/extension/module/d_social_login.php
@@ -71,7 +71,7 @@ class ControllerExtensionModuleDSocialLogin extends Controller
         }
 
         // Scripts
-
+        $this->document->addScript('view/javascript/d_social_login/bootstrap-sortable.js');
         $this->document->addScript('view/javascript/d_bootstrap_tour/js/bootstrap-tour.js');
         $this->document->addStyle('view/javascript/d_bootstrap_tour/css/bootstrap-tour.min.css');
         $this->document->addStyle('view/javascript/d_bootstrap_colorpicker/css/bootstrap-colorpicker.css');
@@ -80,7 +80,6 @@ class ControllerExtensionModuleDSocialLogin extends Controller
         $this->document->addStyle('view/javascript/jquery/magnific/magnific-popup.css');
         $this->document->addScript('view/javascript/d_tinysort/tinysort.js');
         $this->document->addScript('view/javascript/d_tinysort/jquery.tinysort.min.js');
-        $this->document->addScript('view/javascript/d_social_login/bootstrap-sortable.js');
         $this->document->addStyle('view/stylesheet/d_social_login.css');
 
         // Breadcrumbs
